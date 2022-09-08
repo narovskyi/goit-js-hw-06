4aed239b910d071a8e6c34b2f379abc90024bef0
Oleksii Narovskyi <o.narovskyi@gmail.com>
2022-09-08 13:46:30 +0300

    done 2nd task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const arrayOfListItems = ingredients.map(element => {
+  const listItem = document.createElement("li");
+  listItem.textContent = element;
+  listItem.classList.add('item')
+  return listItem;
+});
+
+const ingredientsList = document.querySelector('ul#ingredients');
+ingredientsList.append(...arrayOfListItems);
