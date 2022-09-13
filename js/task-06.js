bf2174c48c0b5a708bda1768a73442c5ca1d8927
Oleksii Narovskyi <o.narovskyi@gmail.com>
2022-09-13 17:28:53 +0300

    done 6th task

---
+++ js/task-06.js
@@ -0,0 +1,11 @@
+const inputField = document.querySelector('input#validation-input');
+
+inputField.addEventListener('blur', () => {
+    if (inputField.value.length === Number(inputField.dataset.length)) {
+        inputField.classList.add('valid');
+        inputField.classList.remove('invalid');
+    } else {
+        inputField.classList.add('invalid');
+        inputField.classList.remove('valid');
+    };1
+})
