34c143eeab17528980fe49c0b9005af9132c3d5a
Oleksii Narovskyi <o.narovskyi@gmail.com>
2022-09-17 21:53:03 +0300

    done 8th task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector('form.login-form');
+const emailInput = document.querySelector('input[name="email"]');
+const passwordInput = document.querySelector('input[name="password"]');
 
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (emailInput.value === "" || passwordInput.value === "") {
+        alert('All fields must be filled');
+    } else {
+        const dataObject = {
+            email: email.value,
+            password: password.value
+        }
+        console.log(dataObject);
+    }
+
+    event.currentTarget.reset();
+})
