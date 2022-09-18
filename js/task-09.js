021dfeaffe67ee980307fecd9865d6f34373e16c
Oleksii Narovskyi <o.narovskyi@gmail.com>
2022-09-18 22:18:18 +0300

    done 9th task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const elemBody = document.querySelector('body');
+const changeColorBtn = document.querySelector('button.change-color');
+const colorCode = document.querySelector('span.color');
+
+changeColorBtn.addEventListener('click', () => {
+  const currentColor = getRandomHexColor();
+  elemBody.style.backgroundColor = currentColor;
+  colorCode.textContent = currentColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
